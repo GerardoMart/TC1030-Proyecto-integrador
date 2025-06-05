34cb7fdecce106e63f43ab9039c94c23ff5b094a
GerardoMart <A01713474@tec.mx>
2025-06-04 21:05:07 -0600

    Add files via upload
    Versión modificada, muestra más datos.

--- titulares.h
+++ titulares.h
@@ -1,11 +1,11 @@
 #ifndef TITULARES_H
 #define TITULARES_H
 
+#include "jugadores.h"
 #include <iostream>
 #include <string>
-#include <cctype> 
+#include <cctype>
 using namespace std;
-
 
 class Titulares : public Jugadores {
 private: 
@@ -22,22 +22,31 @@ public:
     Titulares(int partidos, int gole, int asist, int entrad, int pas, int despe, int ataj, int minutos, 
               string nombre, string tipo, string posicion, int dorsal, string pierna_habil, float sueldo_mensual, int contrato);
 
-    virtual void mostrarInfoJugador() const override;
+    void mostrarInfoJugador() const override;
 
     string estadisticas() const;
     float calcular_rendimiento() const;
     bool tiene_buen_rendimiento() const;
 };
 
-Titulares::Titulares(int partidos, int gole, int asist, int entrad, int pas, int despe, int ataj, int minutos,
-                     string nombre, string tipo, string posicion, int dorsal, string pierna_habil, float sueldo_mensual, int contrato)
+Titulares::Titulares(int partidos, int gole, int asist, int entrad, int pas, int despe, int ataj, int minutos, string nombre, 
+                    string tipo, string posicion, int dorsal, string pierna_habil, float sueldo_mensual, int contrato)
     : Jugadores(nombre, tipo, posicion, dorsal, pierna_habil, sueldo_mensual, contrato),
       titular_en_partidos(partidos), goles(gole), asistencias(asist), entradas(entrad),
       pases(pas), despejes(despe), atajadas(ataj), minutos_jugados(minutos) {}
 
+
 void Titulares::mostrarInfoJugador() const {
-    Jugadores::mostrarInfoJugador();
-    cout << "=== ESTADÍSTICAS ===" << endl;
+    cout << "\n=== INFORMACIÓN DEL JUGADOR TITULAR ===" << endl;
+    cout << "Nombre: " << nombre << endl;
+    cout << "Tipo: " << tipo << endl;
+    cout << "Posición: " << posicion << endl;
+    cout << "Dorsal: " << dorsal << endl;
+    cout << "Pierna hábil: " << pierna_habil << endl;
+    cout << "Sueldo mensual: €" << sueldo_mensual << endl;
+    cout << "Duración del contrato: " << duracion_contrato_meses << " meses" << endl;
+
+    cout << "\n=== ESTADÍSTICAS ===" << endl;
     cout << "Partidos como titular: " << titular_en_partidos << endl;
     cout << "Goles: " << goles << endl;
     cout << "Asistencias: " << asistencias << endl;
